249431474c3227379d293603f7c2c4dfd30b10e4
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2022-11-24 10:37:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1=31;
+let num2=2;
+const multiply=(num1*num2);
+
+let random;
+random = Math.floor(Math.random()*10);
+
+let num3=24;
+let num4=6;
+const mod=(num3/num4);
+
+
+let max;
+max = Math.max(0.5,10,15,20)
 
 // Write your code here
